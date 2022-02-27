22b519fb3f5ee685f24a7cd6c918a3bb5991e59e
ajl2881 <55162949+ajl2881@users.noreply.github.com>
2022-02-26 21:12:03 -0500

    Readded homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <header className="App-header">
+        <title>Raccoon Rex’s Library of Internet Safety</title>
+        <h1>Raccoon Rex’s Library of Internet Safety</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
