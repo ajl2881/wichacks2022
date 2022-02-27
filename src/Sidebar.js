1412efdba259e0da5b0a2fe210ce81fe517c0158
ajl2881 <55162949+ajl2881@users.noreply.github.com>
2022-02-26 23:26:28 -0500

    Trying to add routes/switches/browser again

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import { Route, Switch, Link } from "react-router-dom";
 import './Sidebar.css';
 
 export default props => {
@@ -8,16 +9,23 @@ export default props => {
       <a className="menu-item" href="/">
         Home
       </a>
-
       <a className="menu-item" href="/student">
         Students
       </a>
-      <a className="menu-item" href="/parents">
+      <a className="menu-item" href="/parent">
               Parents
         </a>
       <a className="menu-item" href="/educator">
         Educators
+
+      <Link to="/parent">Parents</Link>
+
       </a>
+      <Switch>
+      <Route path="/parent">
+        <Parents />
+      </Route>
+      </Switch>
     </Menu>
   );
 };
