e6f39de4a9637819bd73642ab0272a8cae2fafcc
ajl2881 <55162949+ajl2881@users.noreply.github.com>
2022-02-26 22:09:10 -0500

    Restructured app home page

--- src/App.js
+++ src/App.js
@@ -5,27 +5,22 @@ import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="App">
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-      <header className="App-header">
-        <title>Raccoon Rex’s Library of Internet Safety</title>
-        <h1>Raccoon Rex’s Library of Internet Safety</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-            <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/educator">Educators</Link>
-                </li>
-                <li>
-                <Link to="/parent">Parents</Link>
-                </li>
-              </ul>
-            </nav>
-      </header>
-    </div>
-    <Switch>
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/educator">Educators</Link>
+            </li>
+            <li>
+            <Link to="/parent">Parent</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+        <Switch>
         <Route exact path="/">
           <Home />
         </Route>
@@ -33,9 +28,10 @@ function App() {
           <Educators />
         </Route>
         <Route path="/parent">
-          <Parents />
+          <Parent />
         </Route>
       </Switch>
+      </div>
   );
 }
 
