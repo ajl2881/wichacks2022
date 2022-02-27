e7926058c72e1deb1a6564c725f0fbbc552c6ec2
ajl2881 <55162949+ajl2881@users.noreply.github.com>
2022-02-26 21:57:28 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <title>Raccoon Rex’s Library of Internet Safety</title>
         <h1>Raccoon Rex’s Library of Internet Safety</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
             <nav>
               <ul id="navigation">
                 <li>
@@ -24,20 +23,19 @@ function App() {
                 </li>
               </ul>
             </nav>
-          </div>
-            <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/educator">
-              <Educators />
-            </Route>
-            <Route path="/parent">
-              <Parents />
-            </Route>
-          </Switch>
       </header>
     </div>
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/educator">
+          <Educators />
+        </Route>
+        <Route path="/parent">
+          <Parents />
+        </Route>
+      </Switch>
   );
 }
 
