3b7a603798059c2e486520585582ac99cc9e6ce1
ajl2881 <55162949+ajl2881@users.noreply.github.com>
2022-02-26 21:48:35 -0500

    Uhh

--- src/App.js
+++ src/App.js
@@ -10,17 +10,32 @@ function App() {
         <title>Raccoon Rex’s Library of Internet Safety</title>
         <h1>Raccoon Rex’s Library of Internet Safety</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/educator">Educators</Link>
+                </li>
+                <li>
+                <Link to="/parent">Parents</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/educator">
+              <Educators />
+            </Route>
+            <Route path="/parent">
+              <Parents />
+            </Route>
+          </Switch>
       </header>
     </div>
   );
